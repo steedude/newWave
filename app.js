3d6f45e2fd76c2f16e7d4182d149ce59ff2d14d1
steedude <85280987+steedude@users.noreply.github.com>
2024-04-29 03:07:38 +0800

    fetch me

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-window.onload = function () {
+window.onload = async function () {
   const world = document.getElementById("world");
+  const response = await fetch("https://api.createHuman.universe.com");
+  const me = await response.json();
+  world.appendChild(me.element);
   const canvas = world.closest(".myEye");
   let intuitionElement = document.getElementById("intuitionInput");
   let planElement = document.getElementById("planInput");
 
   document.addEventListener("keydown", (e) => {
     if (e.key === "PrintScreen") {
-      let mindfulness = Math.random();
+      let mindfulness = me.toFeel(me);
       let plan = howItWorks(planElement.value);
-      let insight = Math.random();
+      let insight = me.toFeel(world);
       let intuition = parseFloat(intuitionElement.value);
       let timing = (Math.random() + insight) / 2;
-      let luck = Math.random();
+      let luck = howUniverseLove(me);
+      // All parameters will range from 0 to 1
       if (
         mindfulness + plan + insight + intuition + timing + luck > 4 ||
         luck > 0.9
