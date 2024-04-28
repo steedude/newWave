e3323aa90583d61256ee81dad8ccd79cf279dce3
steedude <85280987+steedude@users.noreply.github.com>
2024-04-29 02:24:31 +0800

    add input

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 window.onload = function () {
   const world = document.getElementById("world");
   const canvas = world.closest(".myEye");
+  let intuitionElement = document.getElementById("intuitionInput");
+  let planElement = document.getElementById("planInput");
 
   document.addEventListener("keydown", (e) => {
     if (e.key === "PrintScreen") {
       let mindfulness = Math.random();
+      let plan = howItWorks(planElement.value);
       let insight = Math.random();
-      let intuition = Math.random();
-      let timing = Math.random();
+      let intuition = parseFloat(intuitionElement.value);
+      let timing = (Math.random() + insight) / 2;
       let luck = Math.random();
-
       if (
-        mindfulness > 0.8 &&
-        insight > 0.7 &&
-        intuition > 0.8 &&
-        timing > 0.6 &&
-        luck > 0.5
+        mindfulness + plan + insight + intuition + timing + luck > 4 ||
+        luck > 0.9
       ) {
         let jpegUrl = canvas.toDataURL("image/jpeg", 0.9);
         let link = document.createElement("a");
